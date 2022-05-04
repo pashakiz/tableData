c8cfe29fc9eb4e576038cc66fa53124dea45bf81
pashakiz <pashakiz@gmail.com>
2022-05-04 18:23:28 +0300

    launch devserver

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import classNames from 'classnames';
+
+export default class App extends React.Component {
+
+    render() {
+        return (
+            <div className="appp">
+                APP
+            </div>
+        );
+    }
+}
